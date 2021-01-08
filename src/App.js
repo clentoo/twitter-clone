0b7e7f0b8c68505be2818949d19b42d6f8d74be7
Clinton Law <Klintonlaww@gmail.com>
2021-01-08 12:42:11 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Sidebar from './Sidebar';
+
+function App() {
+  //BEM
+  return (
+    <div className="app">
+    <h1>hey clever programmers. Lets build ....</h1>
+
+      {/* Sidebar */}
+      <Sidebar/>
+
+      {/* Feed */}
+
+      {/* Widgets */}
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Sidebar.css";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+          {/* Twitter Icon */}
+          <TwitterIcon/>
+
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+      {/* SidebarOption */}
+
+      {/* Button -> Tweet */}
+    </div>
+  );
+}
+
+export default Sidebar;
